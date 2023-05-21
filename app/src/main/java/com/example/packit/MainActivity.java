f83fc861c9e7f7ec6b418933047ad9c2231e5146
JerzyJerzu <PJ@pawlik.info.pl>
2023-05-21 18:01:38 +0200

    blank

---
+++ app/src/main/java/com/example/packit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.packit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
