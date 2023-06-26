13f5b8eeb274faf67a3ba0287e810d256aada440
JerzyJerzu <PJ@pawlik.info.pl>
2023-06-26 23:41:25 +0200

    added splash screen

---
+++ app/src/main/java/com/example/packit/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.packit;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3000; // Splash screen duration in milliseconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Start a background thread to perform any long-running initialization tasks
+        Thread backgroundThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(SPLASH_DURATION);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // Once the background work is complete, start the main activity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // Close the splash activity to prevent going back to it
+                finish();
+            }
+        });
+        backgroundThread.start();
+    }
+}
